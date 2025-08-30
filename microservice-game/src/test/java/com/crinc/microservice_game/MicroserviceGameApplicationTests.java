512a9528c348db8e6f1f81667e5a21d6989eb502
carlosrs14 <crincones578@gmail.com>
2025-08-29 19:13:23 -0500

    init microservice game

---
+++ microservice-game/src/test/java/com/crinc/microservice_game/MicroserviceGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crinc.microservice_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
