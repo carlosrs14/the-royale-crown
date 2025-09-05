d237c5bfb72348302a4f2b3ee609e53cd6ee5ca1
carlosrs14 <crincones578@gmail.com>
2025-09-05 13:16:27 -0500

    microsrevice gateway

---
+++ microservice-gateway/src/test/java/com/crinc/microservice_gateway/MicroserviceGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.crinc.microservice_gateway;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class MicroserviceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
